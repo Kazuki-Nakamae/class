96372a0aeeccc120ee5d57683d4dfa018985806e
Nakamae_ Kazuki <kazuki_nakamae@hotmail.com>
2016-10-24 22:37:54 +0900

    no message

---
+++ Mathematics_for_Modeling_and_Simulation/ex2/js/RWclass.js
@@ -0,0 +1,273 @@
+/*!
+ * RSclass.js
+ * Copyright 2016 Kazuki Nakamae
+ * Licensed under the MIT license
+ */
+/*[NOTICE]
+* Pseud class-base OOP is implecated in this prigram.
+* You check "http://www.yunabe.jp/docs/javascript_class_in_google.html"
+*/
+//function//////////////////////////////////////////////////////////////////////
+
+/**
+* @function 擬似クラス継承用プロトタイプチェーン指定
+* @param {Function} childCtor  子クラスのコンストラクタ
+* @param {Function} parentCtor 親クラスのコンストラクタ
+*/
+var setInherits=function(childCtor,parentCtor) {
+  Object.setPrototypeOf(childCtor.prototype, parentCtor.prototype);
+};
+
+//randomwalker_class///////////////////////////////////////////////////////////
+/**
+* @class Randomwalker
+*/
+/**
+* @namespace Randomwalker
+*/
+var randomwalker = randomwalker || {};
+/**
+* @constructor
+* @param {Number} trials number of trials
+* @param {Number} xmax value of x domain
+* 定義域は、[-xmax,xmax]とする。
+*/
+randomwalker=function(trials,xmax){
+  //argumentsオブジェクトをArrayオブジェクトに変換
+  var argumentsArr = (arguments.length === 1?[arguments[0]]:Array.apply(null, arguments));
+  try{
+    //入力チェック
+    if(argumentsArr.some(function(element, index, array){
+      return(typeof element === "undefined")
+    })||(argumentsArr.length!=2)
+    ){
+      throw new Error("Input error");
+    }
+    //引数代入
+    this.trials =trials;
+    //walkerの各変数を配列でまとめて宣言
+    this.xArr=Array(this.name);
+    //位置の定義域 (-xmax<x<xmax)とする
+    this.maxX=xmax;
+    //位置のヒストグラム
+    this.histX=[];
+    //位置二乗のヒストグラム
+    this.histXX=[];
+    //前呼び出しでの位置平均
+    this.prevMean=0;
+    //前呼び出しでの位置二乗平均
+    this.prevMeansq=0;
+    //前呼び出しでの位置二乗平均の変化量
+    this.prevMeansqDelta=0;
+  }catch(e){
+    console.error(e);
+  }
+};
+/**
+* @method setRW(inputData,inputDataType,ID)
+* setter
+* @param  {StringGlobalObject_or_Number} inputData 入力データ
+* @param  {StringGlobalObject} inputDataType 入力データの種類
+* @param  {Number} ID 識別番号
+* @return {Boolean} isSuccess 正しい処理がなされたかどうかを返す。(true:正常終了/false:異常終了)
+*/
+randomwalker.prototype.setRW=function(inputData,inputDataType,ID)  {
+  isSuccess=false;
+  try{
+    if(inputDataType==="x"){
+      this.xArr[ID]=inputData;
+    }else{
+      throw new Error("Undefined DataType : "+inputDataType);
+    }
+    isSuccess=true;
+  }catch(e){
+    console.error(e);
+  }finally{
+    return isSuccess;
+  }
+};
+/**
+* @method get(getDataType,ID)
+* getter
+* @param {StringGlobalObject} getDataType   取得データの種類 arguments[0]
+* @param  {Number} ID 識別番号 arguments[1]
+* @return {StringGlobalObject_or_Number_or_null} returnedData  取得したデータ
+*/
+randomwalker.prototype.get=function()  {
+  var returnedData=null;
+  if(arguments[0]==="trials"){
+    returnedData=this.trials;
+  }else if(arguments[0]==="x"){
+    returnedData=this.xArr[arguments[1]];
+  }else if(arguments[0]==="maxX"){
+    returnedData=this.maxX;
+  }else if(arguments[0]==="histX"){
+    returnedData=this.histX;
+  }else if(arguments[0]==="histXX"){
+    returnedData=this.histXX;
+  }
+  return returnedData;
+};
+/**
+* @method calcMean()
+* 全体の平均を算出する
+* @return {Number_or_null} returnedCalc  計算した平均
+*/
+randomwalker.prototype.calcMean=function(){
+  var returnedCalc=null;
+  try{
+    var sum=0
+    for(var i=this.xArr.length-1;i>=0;i--){
+      sum=this.xArr[i]+sum;
+    }
+    returnedCalc=sum/this.xArr.length;
+    if(typeof returnedCalc !== "number"){
+      throw new Error("Calculation Error : "+returnedCalc);
+    }
+  }catch(e){
+    console.error(e);
+  }finally{
+    return(returnedCalc)
+  }
+};
+/**
+* @method calcSD()
+* 位置の標準偏差を算出する
+* @return {Number_or_null} returnedCalc  計算した標準偏差
+*/
+randomwalker.prototype.calcSD=function(){
+  try{
+    var returnedCalc=null;
+    var mean=this.calcMean();
+    var sumsq=0;
+    for(var i=this.xArr.length-1;i>=0;i--){
+      sumsq+=this.xArr[i]*this.xArr[i]
+    }
+    returnedCalc=Math.sqrt((sumsq-this.xArr.length*mean*mean)/this.xArr.length);
+    if(typeof returnedCalc !== "number") throw new Error("Calculation Error : "+returnedCalc);
+  }catch(e){
+    console.error(e);
+  }finally{
+    return(returnedCalc)
+  }
+};
+/**
+* @method calcMeanSquare()
+* 全体の二乗平均を算出する
+* @return {Number_or_null} returnedCalc  計算した二乗平均
+*/
+randomwalker.prototype.calcMeanSquare=function(){
+  try{
+    var returnedCalc=null;
+    var sum=0
+    for(var i=this.xArr.length-1;i>=0;i--){
+      sum=this.xArr[i]*this.xArr[i]+sum
+    }
+    returnedCalc=sum/this.xArr.length;
+  }catch(e){
+    console.error(e);
+  }finally{
+    return(returnedCalc)
+  }
+};
+/**
+* @method calcSD()
+* 位置二乗の標準偏差を算出する
+* @return {Number_or_null} returnedCalc  計算した標準偏差
+*/
+randomwalker.prototype.calcMSSD=function(){
+  try{
+    var returnedCalc=null;
+    var meansquare=this.calcMeanSquare();
+    var sumsq=0;
+    for(var i=this.xArr.length-1;i>=0;i--){
+      sumsq+=this.xArr[i]*this.xArr[i]*this.xArr[i]*this.xArr[i];
+    }
+    returnedCalc=(sumsq-this.xArr.length*meansquare*meansquare)/this.xArr.length;
+    if(typeof returnedCalc !== "number") throw new Error("Calculation Error : "+returnedCalc);
+  }catch(e){
+    console.error(e);
+  }finally{
+    return(returnedCalc)
+  }
+};
+/**
+* @method calcDeltaX()
+* 位置の変化量を計算する
+* @return {Number_or_null} returnedCalc  計算した変化量
+*/
+randomwalker.prototype.calcDeltaX=function(){
+  try{
+    var returnedCalc=null;
+    returnedCalc=this.calcMean()-this.prevMean;
+    this.prevMean=this.calcMean();
+    if(typeof returnedCalc !== "number") throw new Error("Calculation Error : "+returnedCalc);
+  }catch(e){
+    console.error(e);
+  }finally{
+    return(returnedCalc)
+  }
+};
+/**
+* @method calcDeltaXX()
+* 位置二乗の変化量を計算する
+* @return {Array} returnedCalc  計算した位置二乗の変化量;[0]:1次変化量/[1]:2次変化量
+*/
+randomwalker.prototype.calcDeltaXX=function(){
+  var returnedCalcArr=[];
+  returnedCalcArr[0]=this.calcMeanSquare()-this.prevMeansq;
+  this.prevMeansq=this.calcMeanSquare();
+  returnedCalcArr[1]=returnedCalcArr[0]-this.prevMeansqDelta;
+  this.prevMeansqDelta=returnedCalcArr[0];
+  return(returnedCalcArr)
+};
+/**
+* @method updateHist()
+* ヒストグラムを更新する
+* this.histX[index],this.histXX[index]は配列ではなく、
+* indexに負の値をもつことができる連想配列であることに注意
+*/
+randomwalker.prototype.updateHist=function(){
+  try{
+    //位置のヒストグラム
+    for(var x=this.maxX;x>=-this.maxX;x--){
+      this.histX[x]=0;
+    }
+    for(var i=this.xArr.length-1;i>=0;i--){
+      this.histX[this.xArr[i]]=this.histX[this.xArr[i]]+1
+      if(typeof this.histX[this.xArr[i]] !== "number"){
+        throw new Error("Calculation Error : "+this.histX[this.xArr[i]]);
+      }
+    }
+    //位置二乗のヒストグラム
+    for(var x=this.maxX;x>=-this.maxX;x--){
+      this.histXX[x]=0;
+    }
+    for(var i=this.xArr.length-1;i>=0;i--){
+      this.histXX[this.xArr[i]*this.xArr[i]]=this.histXX[this.xArr[i]*this.xArr[i]]+1
+      if(typeof this.histXX[this.xArr[i]*this.xArr[i]] !== "number"){
+        throw new Error("Calculation Error : "+this.histXX[this.xArr[i]*this.xArr[i]]);
+      }
+    }
+  }catch(e){
+    console.error(e);
+  }
+};
+/**
+* @method walk(forwardProb)
+* ウォークを1進める
+* @param  {Number} forwardProb 前に進む確率
+* @param  {Number} time ステップ数(シードで結果を固定するために利用)
+* @param  {Number} controlSeed 可変シードこの値とステップ数を組み合わせてシードを作成,0は禁止
+*/
+randomwalker.prototype.walk=function(forwardProb,time,controlSeed){
+  for(var i=this.xArr.length-1;i>=0;i--){
+    if(i==this.xArr.length-1) Math.seedrandom(i*(time*controlSeed)+time+controlSeed);
+    var randomNum=Math.random();
+    if(randomNum<=forwardProb){
+      this.xArr[i]=this.xArr[i]+1
+    }else{
+      this.xArr[i]=this.xArr[i]-1
+    }
+  }
+};
--- Mathematics_for_Modeling_and_Simulation/ex2/js/main.js
+++ Mathematics_for_Modeling_and_Simulation/ex2/js/main.js
@@ -6,7 +6,13 @@
 
 /*各変数、関数の宣言**************************************************************/
 
-//アニメーション関数を定義
+/**
+* @function requestAnimationFrame
+* アニメーション関数
+* 毎秒60回ループ関数をcallback関数をよびだす
+* 再帰的に使うのが一般的
+* @param {function} callback
+*/
 var requestAnimationFrame
     = window.requestAnimationFrame ||                 // 正式な実装
       window.webkitRequestAnimationFrame ||           // Google Chrome、Safari向け
@@ -14,31 +20,57 @@ var requestAnimationFrame
       window.oRequestAnimationFrame ||                // Opera向け
       window.msRequestAnimationFrame ||               // InternetExplore向け
       function (callback, element) { window.setTimeout(callback, 1000 / 60); }; // 未対応ブラウザ向け
+/**
+* @function onkeydown
+* 自己定義リロードイベント関数
+* Enterでリロードさせる
+* @param {event} e イベント関数
+* ※eはキーボード操作で自動的に発生する
+*/
+document.onkeydown = function (e){
+  if(!e) e = window.event; // レガシー
+  if(e.keyCode == 13){
+    window.location.reload();
+  }
+};
+
 //canvas要素の取得
 var canvasElement = document.getElementById('simulation');
 canvasElement.width = canvasElement.clientWidth;
 canvasElement.height = canvasElement.clientHeight;
+
 //コンテキストの取得
 var context=canvasElement.getContext('2d');       //コンテキストオブジェクト
 var backcontext=document.getElementById('simulation').getContext('2d');	//背景コンテキストオブジェクト
+
 //シミュレーション用変数
-var time=0;
-var width = canvasElement.width;   //横幅
-var height = canvasElement.height; //縦幅
-//ランダムウォーク処理を行うクラスのインスタンスを宣言
-var RWsimulation = new randomwalker(1500);
-for(var i=1500;i>0;i--){
+var time=0; //時間
+var xaxisArr = [-400,-300,-200,-100,0,100,200,300,400];　//x軸
+var taxisArr = [50,100,200,300,400,500,600,700,800,850];
+var meanaxisArr = [50,100,150,200,250,300];
+var meansqaxisArr = [25000,50000,75000,100000,125000,150000];
+var width = canvasElement.width;   //canvas横幅
+var height = canvasElement.height; //canvas縦幅
+var trials=3000;
+
+//ランダムウォーク処理を行うクラスのインスタンスを宣言し、初期座標を入力
+var RWsimulation = new randomwalker(trials,width/2);
+for(var i=trials-1;i>=0;i--){
   RWsimulation.setRW(0,'x',i)
 }
 
 /*メインストリームとなる処理関数の定義***********************************************/
 
-// 読み込み後のwindowイベントの定義
+// 読み込み後のイベントを定義
 window.addEventListener('load', function () {
   resizeTo(canvasElement.height, canvasElement.width); //ウィンドウサイズの指定
   drawStart();        //描画開始関数
 });
-// 描画開始関数の定義
+/**
+* @function drawStart
+* アニメーション開始関数
+* 待ち画面を表示させて、イベントリスナーを登録する
+*/
 var drawStart=function() {
   //開始待ち
   context.save()
@@ -55,71 +87,150 @@ var drawStart=function() {
   context.restore()
 	//クリックでloop()に入るイベントを追加
 	document.documentElement.addEventListener("click", loop,false);
-	//即座にloop()に入るイベントを無効にするイベントを追加
-	document.documentElement.addEventListener('click', function() {
-		document.documentElement.removeEventListener('click', loop, false);
-	}, false);
 }
-//ループ関数の定義
-function loop() {
+/**
+* @function loop
+* アニメーションループ関数
+* requestAnimationFrame()によって再帰的に繰り返される
+* -(初期処理)-計算-描写-データ更新-時間更新-によって構成
+* 10000stepsで終了するようにしてある
+*/
+var loop=function() {
+  //初期処理
+  if(time==0){
+    context.clearRect(0, 0, canvasElement.width, canvasElement.height); //canvasの全領域をクリアー
+    //tの座標
+    context.save();
+    for(var i=xaxisArr.length-1;i>=0;i--){
+      context.beginPath();
+      context.moveTo(taxisArr[i],height-20);
+      context.lineTo(taxisArr[i],height-10);
+      context.lineTo(taxisArr[i],height-10);
+      context.fill();
+      context.textAlign="center";
+      context.font="12px 'Times New Roman'";
+      context.fillText(taxisArr[i],taxisArr[i],height-1)
+    }
+    context.font="20px 'Times New Roman'";
+    context.fillText("steps",880,height-5)
+    context.restore();
+    //<x>の座標
+    context.save();
+    context.strokeStyle="blue"
+    context.fillStyle="blue"
+    for(var i=xaxisArr.length-1;i>=0;i--){
+      context.beginPath();
+      context.moveTo(20,height-meanaxisArr[i]);
+      context.lineTo(10,height-meanaxisArr[i]);
+      context.lineTo(10,height-meanaxisArr[i]);
+      context.fill();
+      context.textAlign="center";
+      context.font="12px 'Times New Roman'";
+      context.fillText(meanaxisArr[i],10,height-meanaxisArr[i])
+    }
+    context.font="20px 'Times New Roman'";
+    context.fillText("<x>",20,height-270)
+    context.restore();
+    //log(<x^2>)の座標
+    context.save();
+    context.strokeStyle="blue"
+    context.fillStyle="green"
+    for(var i=xaxisArr.length-1;i>=0;i--){
+      context.textAlign="center";
+      context.font="12px 'Times New Roman'";
+      context.fillText(meansqaxisArr[i],width-25,height-meansqaxisArr[i]/500)
+    }
+    context.font="20px 'Times New Roman'";
+    context.fillText("<x^2>",860,height-270)
+    context.restore();
+  }
 
-  //計算
-  var average,sum=0
-  for(var i=1500;i>0;i--){
-    sum=RWsimulation.getRW('x',i)+sum
-  }
-  average=Math.round10(sum/1500,-2)
-  var hist=[]
-  for(var i=450;i>-449;i--){
-    hist[i]=0;
-  }
-  for(var i=1500;i>0;i--){
-    hist[RWsimulation.getRW('x',i)]=hist[RWsimulation.getRW('x',i)]+1
-  }
 
-  //描写
+  //the part of Calculation
+  var mean=Math.round10(RWsimulation.calcMean(),-2); //<x>
+  var sdx=Math.round10(RWsimulation.calcSD(),-2); //S.D. of <x>
+  var meansq=Math.round10(RWsimulation.calcMeanSquare(),-2); //<x^2>
+  var deltaX=Math.round10(RWsimulation.calcDeltaX(),-1);
+  var deltaXXArr=RWsimulation.calcDeltaXX()
+  var deltaXX=Math.round10(deltaXXArr[0],-1);
+  var ddeltaXX=Math.round10(deltaXXArr[1],-1);
+  RWsimulation.updateHist();  //ヒストグラム更新
+
+
+  //the part of Drawing
   if((time%1)==0){
-    context.clearRect(0, 0, width, height); //canvasの全領域をクリアー
+    context.clearRect(0, 0, width, height/2); //canvasの上半分をクリアー
+    context.save();
     context.textAlign="start";
+    context.font="20px 'Times New Roman'";
     context.fillText("Steps="+time,5,30)
-    context.fillText("Average="+average,5,60)
+    context.fillStyle="blue"
+    context.fillText("<x>="+mean+" ± "+sdx,5,60)
+    context.fillText("Δ<x>="+deltaX,5,90)
+    context.fillStyle="green"
+    context.fillText("<x^2>="+meansq,5,120)
+    context.fillText("Δ<x^2>="+deltaXX,5,150)
+    context.fillText("ΔΔ<x^2>="+Math.abs(ddeltaXX).toExponential(1),5,180)
+    context.restore();
+    //each x
     context.save()
     context.strokeStyle="red"
-    context.globalAlpha=0.1
-    for(var i=1500;i>0;i--){
+    context.globalAlpha=0.3;
+    for(var i=1499;i>=0;i--){
       context.beginPath()
-      context.moveTo(RWsimulation.getRW('x',i)+width/2, 0)
-      context.lineTo(RWsimulation.getRW('x',i)+width/2, height*2/3)
+      context.moveTo(RWsimulation.get('x',i)+width/2, 0)
+      context.lineTo(RWsimulation.get('x',i)+width/2, height/2-10)
       context.stroke()
     }
+    context.restore()
+    //x axis
+    context.save()
+    for(var i=xaxisArr.length-1;i>=0;i--){
+      context.beginPath();
+      context.moveTo(xaxisArr[i]+width/2,height/2-20);
+      context.lineTo(xaxisArr[i]+1+width/2,height/2-10);
+      context.lineTo(xaxisArr[i]-1+width/2,height/2-10);
+      context.fill();
+      context.textAlign="center";
+      context.font="12px 'Times New Roman'";
+      context.fillText(xaxisArr[i],xaxisArr[i]+width/2,height/2-1)
+    }
+    context.restore()
     //histogram
-    for(var i=450;i>-449;i--){
+    for(var i=RWsimulation.get('maxX');i>=-RWsimulation.get('maxX');i--){
       context.beginPath()
-      context.moveTo(i+width/2, height*2/3-hist[i])
-      context.lineTo(i+width/2, height*2/3)
+      context.moveTo(i+width/2, height/2-10-RWsimulation.get('histX')[i])
+      context.lineTo(i+width/2, height/2-10)
       context.stroke()
-      context.restore();
     }
-    //average point
+    //The point of mean
     context.beginPath();
-    context.moveTo(average+width/2,height*2/3);
-    context.lineTo(average+10+width/2,height*2/3+50);
-    context.lineTo(average-10+width/2,height*2/3+50);
+    context.moveTo(mean+width/2,height/2-10);
+    context.lineTo(mean+10+width/2,height/2-1);
+    context.lineTo(mean-10+width/2,height/2-1);
     context.fill();
+    //t-<x> plot
+    context.save();
+    context.strokeStyle="blue"
+    context.beginPath();
+    context.arc(time*1, height-mean, 0.01, 0, Math.PI*2, false)
+    context.stroke();
+    context.restore();
+    //t-<x^2> plot
+    context.save();
+    context.strokeStyle="green"
+    context.beginPath();
+    context.arc(time*1, height-meansq/500, 0.01, 0, Math.PI*2, false)
+    context.stroke();
+    context.restore();
   }
 
-  //遷移
-  for(var i=1500;i>0;i--){
-    var randomNum=Math.random()*3;
-    if(randomNum>=1){
-      RWsimulation.setRW(RWsimulation.getRW('x',i)+1,'x',i)
-    }else{
-      RWsimulation.setRW(RWsimulation.getRW('x',i)-1,'x',i)
-    }
-  }
+  //the part of transition
+  var controlSeed=1;
+  RWsimulation.walk(2/3,time,controlSeed);
 
-  //時間経過
+  //time proceeding
   time++;
 
-  if(time!=10000)	requestAnimationFrame(loop);     //loop関数の呼び出し
+  if(time!=10000) requestAnimationFrame(loop);     //loop関数の呼び出し
 }
